57a51869fa5c5ae4c5e35c6ff7b37621db6a63d1
Khushi Maurya <khushimaurya5498@gmail.com>
2026-03-23 16:04:56 +0530

    fixed port binding

--- app.py
+++ app.py
@@ -128,5 +128,8 @@ def save_contact():
         print("Error:", e)
         return jsonify({'success': False, 'error': str(e)})
     
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
